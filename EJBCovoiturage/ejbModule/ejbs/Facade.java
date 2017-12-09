d67abf9b4f4863a6f25aeca9925e646738922835
Manuel ORY <manuory@live.fr>
2017-12-09 17:56:08 +0100

    test push sur nouveau dépôt + ajout du package EJB

---
+++ EJBCovoiturage/ejbModule/ejbs/Facade.java
@@ -0,0 +1,15 @@
+package ejbs;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class Facade
+ */
+@Stateless
+@LocalBean
+public class Facade {
+
+
+
+}
